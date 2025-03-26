819530222d60d083402402c41ac7babac2590d65
adwitafernanda <adwitafernanda4@gmail.com>
2025-03-26 21:21:23 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,22 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Todo; // Pastikan model Todo diimpor
+use Illuminate\Support\Facades\Auth; // Tambahkan ini untuk Auth
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    public function index() {
+    public function index(){
+        $todos = Todo::all();
+        //$todos = Todo::where('user_id', Auth::id())->get();
+        dd($todos);
         return view('todo.index');
     }
 
-    public function create()
-    {
-        return view('todo.create');
+    public function create(){
+        return view("todo.create");
     }
-
-    public function edit()
-    {
-        return view('todo.edit');
+    public function edit(){
+        return view("todo.edit");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
